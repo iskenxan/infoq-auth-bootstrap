1f34b018414de3723ebf7e5b89335499299b06b6
Iskander Samatov <iskenxan11@gmail.com>
2021-06-13 12:30:42 -0500

    setup auth trigger

--- src/containers/Auth.js
+++ src/containers/Auth.js
@@ -1,9 +1,18 @@
+import { useAuth0 } from '@auth0/auth0-react'
 import React from 'react'
 
 export default function Auth() {
+    const { loginWithRedirect } = useAuth0()
+
+    const handleSignIn = () => {
+        loginWithRedirect()
+    }
+
     return (
-        <div>
+        <div style={{ display: "flex", flexDirection: 'column', alignItems: 'center' }}>
             Auth
+            <button onClick={handleSignIn} style={{ maxWidth: 120 }}>Sign in</button>
         </div>
     )
 }
+
